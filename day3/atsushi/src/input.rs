aebdf298b38784e22fba5e501f022515186c461a
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-03 15:36:59 +0000

    Specify by part

--- day3/atsushi/src/input.rs
+++ day3/atsushi/src/input.rs
@@ -4,18 +4,18 @@ use std::{env, str::Lines};
 #[derive(Debug)]
 enum InputError {
     FileDoesntExist,
-    Need3Args,
+    Need4Args,
 }
 
 fn read_input() -> Lines<'static> {
     let args: Vec<String> = env::args().collect();
     let lines = match args.len() as u8 {
-        3 => match args[2].as_str() {
+        4 => match args[2].as_str() {
             "test" => Ok(include_str!("../inputs/test.txt")),
             "puzzle" => Ok(include_str!("../inputs/puzzle.txt")),
             _ => Err(InputError::FileDoesntExist),
         },
-        _ => Err(InputError::Need3Args),
+        _ => Err(InputError::Need4Args),
     }
     .unwrap()
     .lines();
--- day3/atsushi/src/main.rs
+++ day3/atsushi/src/main.rs
@@ -1,14 +1,26 @@
+use std::env;
+
 use atsushi::input::read_file;
 
 fn main() {
+    let part: Vec<String> = env::args().collect();
     let rucksacks = read_file();
-    let priority_sum: Option<u32> = rucksacks
-        .into_iter()
-        .map(|r| r.get_missing_item_score())
-        .reduce(|tot, sum| tot + sum);
 
-    match priority_sum {
-        Some(v) => println!("Part1 total of priority items: {}", v),
-        None => panic!("No total"),
+    match part[3].parse().unwrap() {
+        1 => {
+            let priority_sum: Option<u32> = rucksacks
+                .into_iter()
+                .map(|r| r.get_missing_item_score())
+                .reduce(|tot, sum| tot + sum);
+
+            match priority_sum {
+                Some(v) => println!("Part1 total of priority items: {}", v),
+                None => panic!("No total"),
+            }
+        }
+        2 => {
+            panic!("TODO")
+        }
+        _ => panic!("Has to be 1 or 2"),
     }
 }
