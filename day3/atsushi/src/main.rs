531705c264d01bcd7dceb4c68216a486c14cb8f0
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-03 15:18:29 +0000

    Solve part 1

--- day3/atsushi/src/main.rs
+++ day3/atsushi/src/main.rs
@@ -2,8 +2,13 @@ use atsushi::input::read_file;
 
 fn main() {
     let rucksacks = read_file();
+    let priority_sum: Option<u32> = rucksacks
+        .into_iter()
+        .map(|r| r.get_missing_item_score())
+        .reduce(|tot, sum| tot + sum);
 
-    for r in rucksacks {
-        println!("{:?}", r);
+    match priority_sum {
+        Some(v) => println!("Part1 total of priority items: {}", v),
+        None => panic!("No total"),
     }
 }
