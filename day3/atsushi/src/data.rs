2348b24e13ad86a04f4c4d7361612eb4f82adb49
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-03 15:18:21 +0000

    Implement priority item getter for each rucksack

--- day3/atsushi/src/data.rs
+++ day3/atsushi/src/data.rs
@@ -1,9 +1,13 @@
-use std::str::FromStr;
-use std::string::ParseError;
+use std::{result::Result, str::FromStr, string::ParseError};
 
 #[derive(Debug)]
 pub struct Rucksack {
     compartments: [String; 2],
+}
+
+#[derive(Debug)]
+enum RucksackErr {
+    NoCommonItem,
 }
 
 impl FromStr for Rucksack {
@@ -17,5 +21,32 @@ impl FromStr for Rucksack {
         Ok(Rucksack {
             compartments: [first, second],
         })
+    }
+}
+
+impl Rucksack {
+    fn get_score(&self, c: char) -> u32 {
+        let index = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            .find(c)
+            .unwrap() as u32;
+
+        index + 1
+    }
+
+    fn find_common_item(&self) -> Result<char, RucksackErr> {
+        let first = self.compartments[0].chars();
+        let second = &self.compartments[1];
+
+        for c in first {
+            if second.contains(c) {
+                return Ok(c);
+            }
+        }
+        Err(RucksackErr::NoCommonItem)
+    }
+
+    pub fn get_missing_item_score(self: &Self) -> u32 {
+        let common_item = self.find_common_item().unwrap();
+        self.get_score(common_item)
     }
 }
