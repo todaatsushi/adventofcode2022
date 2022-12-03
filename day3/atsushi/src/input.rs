820415f6fd03536cf4dc3a5c76651e2c95f81c19
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-03 14:38:13 +0000

    Read file into rucksacks

---
+++ day3/atsushi/src/input.rs
@@ -0,0 +1,32 @@
+use crate::data::Rucksack;
+use std::{env, str::Lines};
+
+#[derive(Debug)]
+enum InputError {
+    FileDoesntExist,
+    Need3Args,
+}
+
+fn read_input() -> Lines<'static> {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let lines = match args.len() as u8 {
+        3 => match args[2].as_str() {
+            "test" => Ok(include_str!("../inputs/test.txt")),
+            "puzzle" => Ok(include_str!("../inputs/puzzle.txt")),
+            _ => Err(InputError::FileDoesntExist),
+        },
+        _ => Err(InputError::Need3Args),
+    }
+    .unwrap()
+    .lines();
+
+    lines
+}
+
+pub fn read_file() -> Vec<Rucksack> {
+    let input = read_input();
+    let rucksacks: Vec<Rucksack> = input.map(str::parse).map(Result::unwrap).collect();
+
+    rucksacks
+}
