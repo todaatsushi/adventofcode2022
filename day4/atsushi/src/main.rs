e5cf84eff822de5f096e73928831050dec5d9770
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-04 15:44:27 +0000

    Refactor to use dynamic arr sizes

--- day4/atsushi/src/main.rs
+++ day4/atsushi/src/main.rs
@@ -1,37 +1,51 @@
 use atsushi::data::read_input;
 
-fn get_section_vec(pair: Vec<&str>) -> [[u8; 9]; 2] {
+fn get_section_vec(pair: Vec<&str>, arr_len: i8, offset: i8) -> [Vec<i8>; 2] {
     let vals = pair
         .iter()
         .map(|p| {
             p.split("-")
-                .map(|n| n.parse::<u8>().unwrap())
-                .collect::<Vec<u8>>()
+                .map(|n| n.parse::<i8>().unwrap())
+                .collect::<Vec<i8>>()
         })
         .map(|p| {
-            let mut sec: [u8; 9] = [0; 9];
+            let mut sec: Vec<i8> = vec![0; arr_len as usize];
 
             for mut i in p[0]..p[1] + 1 {
-                i -= 1;
+                i -= offset;
                 sec[i as usize] += 1;
             }
             sec
         })
         .collect::<Vec<_>>();
 
-    if vals[0].iter().sum::<u8>() > vals[1].iter().sum::<u8>() {
-        [vals[0], vals[1]]
+    if vals[0].iter().sum::<i8>() > vals[1].iter().sum::<i8>() {
+        [vals[0].clone(), vals[1].clone()]
     } else {
-        [vals[1], vals[0]]
+        [vals[1].clone(), vals[0].clone()]
     }
 }
 
-fn parse_file() -> Vec<[[u8; 9]; 2]> {
+fn parse_file() -> Vec<[Vec<i8>; 2]> {
     let raw_pairs = read_input()
         .into_iter()
         .map(|l| l.split(",").into_iter().collect::<Vec<&str>>())
-        .map(|p| get_section_vec(p));
-    raw_pairs.collect::<Vec<[[u8; 9]; 2]>>()
+        .map(|pair| {
+            let vals = pair
+                .iter()
+                .map(|p| {
+                    p.split("-")
+                        .map(|n| n.parse::<i8>().unwrap())
+                        .collect::<Vec<i8>>()
+                })
+                .collect::<Vec<_>>();
+            let min = &vals.iter().flatten().min().unwrap();
+            let max = &vals.iter().flatten().max().unwrap();
+            let arr_len = (**min - **max).abs() + 1;
+
+            get_section_vec(pair, arr_len, **min)
+        });
+    raw_pairs.collect::<Vec<[Vec<i8>; 2]>>()
 }
 
 fn main() {
@@ -40,7 +54,9 @@ fn main() {
     let section_pairs = parse_file();
 
     for sp in &section_pairs {
-        for i in 0..9 {
+        let arr_len = &sp[0].len();
+
+        for i in 0..*arr_len {
             if sp[1][i] == 1 && sp[0][i] != 1 {
                 non_overlapping_pairs += 1;
                 break;
