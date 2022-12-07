d4d2e71b046324957be2e792ee377982aa6d9d93
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-07 18:33:46 +0000

    Part 1

--- day6/atsushi/solve.py
+++ day6/atsushi/solve.py
@@ -2,7 +2,25 @@ import sys
 
 from src import input
 
+
+def get_index_of_first_marker(string: str) -> int:
+    left, right = 0, 4
+
+    while left < len(string) - 1:
+        sub_string = string[left:right]
+
+        if len(set(sub_string)) == 4:
+            return right
+
+        left += 1
+        right += 1
+
+    raise Exception("Expected a startpoint.")
+
+
 if __name__ == "__main__":
     file = sys.argv[1]
 
-    print(input.load(file))
+    for string in input.load(file):
+        first_marker_index = get_index_of_first_marker(string)
+        print(f"Part 1: {first_marker_index}")
