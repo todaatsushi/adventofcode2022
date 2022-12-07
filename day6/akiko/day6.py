f70f55d36cbd7a0640284f8e838118e45408eb53
Akiko <nope>
2022-12-07 09:12:30 +0000

    day6 wip

--- day6/akiko/day6.py
+++ day6/akiko/day6.py
@@ -1,11 +1,21 @@
-data = ['mjqjpqmgbljsphdztnvjfqwrcgsmlb']
+data = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 
 marker_list = ''
 for i in data:
     # i = 'm'
+    print(i)
     if len(marker_list) == 4:
-        print(marker_list)
+        marker_set = set(marker_list)
+        if len(marker_set) == 4:
+            print('hello')
+            break
     
     else:
-            marker_list = marker_list + i
+        marker_list = marker_list + i
     # print(marker_list)break 
+
+print(marker_set)
+
+# start with empty string
+# append letters from input
+# when length of string is 4, check to see that 
