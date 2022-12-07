941dc958a1608128e5d1db59883cd721e0dd11f4
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-07 17:53:22 +0000

    Load data

--- day5/atsushi/solve.py
+++ day5/atsushi/solve.py
@@ -1,2 +1,10 @@
+import sys
+
+from src import input
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    file = sys.argv[1]
+    stack, moves = input.load(file)
+
+    print(stack, moves)
