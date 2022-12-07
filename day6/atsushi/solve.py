9c8279925ce84a952b186ea5c9566bac62e6158f
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-07 18:34:56 +0000

    Refactor for parts

--- day6/atsushi/solve.py
+++ day6/atsushi/solve.py
@@ -3,13 +3,13 @@ import sys
 from src import input
 
 
-def get_index_of_first_marker(string: str) -> int:
-    left, right = 0, 4
+def get_index_of_first_marker(string: str, num_unique_chars: int) -> int:
+    left, right = 0, num_unique_chars
 
     while left < len(string) - 1:
         sub_string = string[left:right]
 
-        if len(set(sub_string)) == 4:
+        if len(set(sub_string)) == num_unique_chars:
             return right
 
         left += 1
@@ -20,7 +20,9 @@ def get_index_of_first_marker(string: str) -> int:
 
 if __name__ == "__main__":
     file = sys.argv[1]
+    part = int(sys.argv[2])
+    num_unique_chars = {1: 4, 2: 14}[part]
 
     for string in input.load(file):
-        first_marker_index = get_index_of_first_marker(string)
-        print(f"Part 1: {first_marker_index}")
+        first_marker_index = get_index_of_first_marker(string, num_unique_chars)
+        print(f"Part {part}: {first_marker_index}")
