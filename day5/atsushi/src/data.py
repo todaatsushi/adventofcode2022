005c4ec3ea57d8330bc3bd09a4de9bdf1736ed57
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-07 17:52:06 +0000

    create stack which holds the placement of the vals

--- day5/atsushi/src/data.py
+++ day5/atsushi/src/data.py
@@ -13,3 +13,32 @@ class Move:
     def new(cls, line: str) -> Move:
         parts = line.strip().split(" ")
         return Move(int(parts[3]), int(parts[-1]), int(parts[1]))
+
+
+class Stack:
+    def __init__(self) -> None:
+        self.items: list[list[str]] = []
+
+    def __repr__(self) -> str:
+        return f"<Stack items={self.items}>"
+
+    @classmethod
+    def load_from_input(cls, input: str) -> Stack:
+        stack = cls()
+        cols, *items = input.split("\n")[::-1]
+
+        for c in cols.split(" "):
+            if c:
+                stack.items.append([])
+
+        for row in items:
+            start, end, col_num = 0, 3, 0
+            row = list(row)
+            while start < len(row) - 1:
+                vals = row[start:end]
+                if any(vals) and vals[1].strip():
+                    stack.items[col_num].append(vals[1])
+                start = end + 1
+                end = start + 3
+                col_num += 1
+        return stack
