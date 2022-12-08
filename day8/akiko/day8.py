2ed6dd368697f42988ebbe2a405d3943ca87f475
Akiko <nope>
2022-12-08 14:54:37 +0000

    almost there part 1 day8

--- day8/akiko/day8.py
+++ day8/akiko/day8.py
@@ -1,9 +1,3 @@
-def max_in_range(list_of_values, min_index, max_index):
-    comparison_values = []
-    for i in range(min_index, max_index):
-        comparison_values.append(list_of_values[i])
-    return max(comparison_values)
-
 def extract_row(grid, location):
     return grid[location]
 
@@ -14,6 +8,50 @@ def extract_col(grid, location):
         col_list.append(grid[i][location])
     return col_list
 
+def max_in_range(list_of_values, starting_index, max_index):
+    comparison_values = []
+    for i in range(starting_index, max_index+1):
+        comparison_values.append(list_of_values[i])
+    return max(comparison_values)
+
+def is_tree_visible_horizontally(grid, row_index, column_index):
+    row = extract_row(grid, row_index)
+    tree = row[column_index]
+    max_row_index = len(row)-1
+    # trees at edge are always visible
+    if column_index == max_row_index or column_index == 0:
+        return True
+    # check to right of grid column location within my row
+    max_tree_to_right = max_in_range(row,column_index+1, max_row_index)
+    if tree > max_tree_to_right:
+        return True
+    # check to left of grid column location within my row
+    max_tree_to_left = max_in_range(row, 0, column_index-1)
+    if tree > max_tree_to_left:
+        return True
+    return False
+
+def is_tree_visible_vertically(grid, row_index, column_index):
+    column = extract_col(grid, column_index)
+    tree = column[row_index]
+    max_column_index = len(column)-1
+    # trees at edge are always visible
+    if row_index == max_column_index or row_index == 0:
+        return True
+    # check to right of grid row location within my column
+    max_tree_to_right = max_in_range(column,row_index+1, max_column_index)
+    if tree > max_tree_to_right:
+        return True
+    # check to left of grid row location within my column
+    max_tree_to_left = max_in_range(column, 0, row_index-1)
+    if tree > max_tree_to_left:
+        return True
+    return False
+
+def is_tree_visible(grid, row_index, column_index):
+    return is_tree_visible_horizontally(grid, row_index, column_index) or is_tree_visible_vertically(grid, row_index, column_index)
+
+
 grid = [
     [3,0,3,7,3],
     [2,5,5,1,2],
@@ -22,24 +60,10 @@ grid = [
     [3,5,3,9,0]
 ]
 
-row = 2
-col = 3
-
-print(extract_row(grid, row))
-print(extract_col(grid, col))
-# row = extract_row(grid, 1)
-# print(max_in_range(row, 3, len(row)))
-
-# col = extract_col(grid, 1)
-# print(col)
-# print(max_in_range(col, 3, len(col)))
-
-# row = [3,5,3,9,0]
-# print(horizontal_max(row, 0, 2))
-# print(horizontal_max(row, 2, len(row)))
-
-# len(grid) -> max_row
-
-# extract row 
-
-# extract column
+counter = 0
+for row_index in range(0, len(grid)): 
+    for col_index in range(0, len(grid[row_index])):
+        # print(row_index, col_index)
+        if is_tree_visible(grid, row_index, col_index) == True:
+            counter += 1
+print(counter)
