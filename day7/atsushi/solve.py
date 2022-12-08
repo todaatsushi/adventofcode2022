1b4fe8e5ccbb310c4de5aeb5e58745e6efc96dbb
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-08 19:02:36 +0000

    Solve part 1

--- day7/atsushi/solve.py
+++ day7/atsushi/solve.py
@@ -2,16 +2,13 @@ import sys
 
 from src import input
 
-
 if __name__ == "__main__":
     file = sys.argv[1]
     part = int(sys.argv[2])
 
     commands = input.read(file)
     folder = input.load(commands)
-
-    print(f"Part {2}: {file}")
-
-    import ipdb
-
-    ipdb.set_trace(context=40)
+    folder.get_size()
+    folders = folder.get_folders_with_max_size(100000, set())
+    total = sum([x[1] for x in folders])
+    print(f"Part {part}: {total}")
--- day7/atsushi/src/data.py
+++ day7/atsushi/src/data.py
@@ -8,9 +8,35 @@ class File:
     name: str
     size: int
 
+    def get_size(self) -> int:
+        return self.size
+
 
 @dataclasses.dataclass
 class Folder:
     name: str
-    contents: dict[str, "Folder"]
+    contents: dict[str, "Folder" | File]
     parent: "Folder" | None
+    size: int | None = None
+
+    def get_size(self) -> int:
+        if not self.contents:
+            if self.size is None:
+                return 0
+            return self.size
+
+        total = sum([f.get_size() for f in self.contents.values()])
+        self.size = total
+        return self.size
+
+    def get_folders_with_max_size(
+        self, size: int, folders: set[tuple[str, int]]
+    ) -> set[tuple[str, int]]:
+        if self.size and self.size <= size:
+            folders.add((self.name, self.size))
+
+        for item in self.contents.values():
+            if isinstance(item, Folder):
+                folders = folders.union(item.get_folders_with_max_size(size, folders))
+
+        return folders
