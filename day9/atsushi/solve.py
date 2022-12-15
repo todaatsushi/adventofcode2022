a3b1714ed67827f7fdc4f77c66ff27d79e9913f5
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-15 20:25:25 +0000

    Solve part 1

--- day9/atsushi/solve.py
+++ day9/atsushi/solve.py
@@ -1,5 +1,16 @@
 import sys
 
+from src import input, move
+
 if __name__ == "__main__":
-    part = sys.argv[2]
+    part = int(sys.argv[2])
     file = sys.argv[1]
+
+    if part == 1:
+        directions = input.load(file)
+        head = move.apply(directions)
+        tail_visited = len(head.tail.visited)
+    else:
+        raise NotImplementedError
+
+    print(f"Part {part}: {tail_visited}")
