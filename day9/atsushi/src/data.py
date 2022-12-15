da0ec7922ee1f908993acacb5aa6ff7bbc884a32
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-15 19:40:05 +0000

    Rope parts

--- day9/atsushi/src/data.py
+++ day9/atsushi/src/data.py
@@ -1,8 +1,8 @@
 import enum
-from abc import ABC
 from typing import TypeAlias
 
 Moves: TypeAlias = tuple[tuple["Direction", int]]
+Coordinate: TypeAlias = tuple[int, int]
 
 
 class Direction(enum.Enum):
@@ -10,3 +10,40 @@ class Direction(enum.Enum):
     RIGHT = "R"
     DOWN = "D"
     LEFT = "L"
+
+
+MODIFIERS = {
+    Direction.RIGHT: (1, 0),
+    Direction.LEFT: (-1, 0),
+    Direction.DOWN: (0, -1),
+    Direction.UP: (0, 1),
+}
+
+
+class Knot:
+    position: Coordinate
+
+    def __init__(self, position: Coordinate) -> None:
+        self.position = position
+
+
+class Tail(Knot):
+    visited: set[Coordinate]
+
+
+class Head(Knot):
+    tail: Tail
+
+    def __init__(self, position: Coordinate) -> None:
+        super().__init__(position)
+        self.tail = Tail(position)
+
+    def move(self, direction: Direction, distance: int) -> None:
+        modifier = MODIFIERS[direction]
+
+        while distance > 0:
+            self.position = (
+                self.position[0] + modifier[0],
+                self.position[1] + modifier[1],
+            )
+            distance -= 1
