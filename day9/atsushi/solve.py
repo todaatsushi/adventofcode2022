a0603c800eb341f98b5300c4856e433ff9f501d0
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-16 18:34:48 +0000

    Implement part 2

--- day9/atsushi/solve.py
+++ day9/atsushi/solve.py
@@ -6,11 +6,15 @@ if __name__ == "__main__":
     part = int(sys.argv[2])
     file = sys.argv[1]
 
+    directions = input.load(file)
     if part == 1:
-        directions = input.load(file)
-        head = move.apply(directions)
+        head = move.apply(directions, 1)
         tail_visited = len(head.tail.visited)
     else:
-        raise NotImplementedError
+        head = move.apply(directions, 9)
+        t = head.tail
+        while t.tail:
+            t = t.tail
+        tail_visited = len(t.visited)
 
     print(f"Part {part}: {tail_visited}")
