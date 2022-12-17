9495b503391154b22ab49fd0d94d2660964b72d7
Atsushi Toda <todaatsushi0@gmail.com>
2022-12-17 00:05:23 +0000

    Tests

--- day9/atsushi/src/tests.py
+++ day9/atsushi/src/tests.py
@@ -4,45 +4,12 @@ from src import data, input, move
 
 
 @pytest.mark.parametrize(
-    "old, new, expected",
-    (
-        (
-            (1, 1),
-            (0, 1),
-            data.Direction.LEFT,
-        ),
-        (
-            (1, 1),
-            (2, 1),
-            data.Direction.RIGHT,
-        ),
-        (
-            (1, 1),
-            (1, 2),
-            data.Direction.UP,
-        ),
-        (
-            (1, 1),
-            (1, 0),
-            data.Direction.DOWN,
-        ),
-    ),
-)
-def test_get_new_direction(
-    old: data.Coordinate, new: data.Coordinate, expected: data.Direction
-) -> None:
-    res = data.get_new_direction(old, new)
-
-    assert res == expected
-
-
-@pytest.mark.parametrize(
     "file, tails, expected",
     (
         ("inputs/test.txt", 1, 13),
         ("inputs/puzzle.txt", 1, 6098),
         ("inputs/test.txt", 9, 1),
-        ("inputs/puzzle.txt", 9, 0),
+        ("inputs/puzzle.txt", 9, 2597),
     ),
 )
 def test_day(file: str, tails: int, expected: int) -> None:
@@ -55,3 +22,102 @@ def test_day(file: str, tails: int, expected: int) -> None:
     assert isinstance(t, data.Tail)
 
     assert len(t.visited) == expected
+
+
+@pytest.mark.parametrize(
+    "moves, tails, expected",
+    (
+        (
+            ((data.Direction.UP, 1),),
+            1,
+            (
+                (0, 1),
+                (0, 0),
+            ),
+        ),
+        (
+            ((data.Direction.LEFT, 1),),
+            1,
+            (
+                (-1, 0),
+                (0, 0),
+            ),
+        ),
+        (
+            ((data.Direction.RIGHT, 1),),
+            1,
+            (
+                (1, 0),
+                (0, 0),
+            ),
+        ),
+        (
+            ((data.Direction.DOWN, 1),),
+            1,
+            (
+                (0, -1),
+                (0, 0),
+            ),
+        ),
+        (
+            ((data.Direction.RIGHT, 4),),
+            1,
+            (
+                (4, 0),
+                (3, 0),
+            ),
+        ),
+        (
+            ((data.Direction.DOWN, 10),),
+            1,
+            (
+                (0, -10),
+                (0, -9),
+            ),
+        ),
+        (
+            (
+                (data.Direction.UP, 3),
+                (data.Direction.RIGHT, 4),
+            ),
+            1,
+            (
+                (4, 3),
+                (3, 3),
+            ),
+        ),
+        (
+            (
+                (data.Direction.UP, 1),
+                (data.Direction.RIGHT, 1),
+                (data.Direction.RIGHT, 1),
+            ),
+            1,
+            (
+                (2, 1),
+                (1, 1),
+            ),
+        ),
+        (
+            ((data.Direction.UP, 1), (data.Direction.RIGHT, 1), (data.Direction.UP, 1)),
+            1,
+            (
+                (1, 2),
+                (1, 1),
+            ),
+        ),
+    ),
+)
+def test_move(
+    moves: tuple[tuple[data.Direction, int]],
+    tails: int,
+    expected: tuple[data.Coordinate],
+) -> None:
+    head = move.apply(moves, tails)
+
+    i = 0
+    current = head
+    while current.tail:
+        assert current.position == expected[i]
+        i += 1
+        current = current.tail
