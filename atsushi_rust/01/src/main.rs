7400413bfed91ed4b744ec3c1137accf7288f5a3
Atsushi Toda <todaatsushi0@gmail.com>
2023-02-17 18:10:46 +0000

    Part 2

--- atsushi_rust/01/src/main.rs
+++ atsushi_rust/01/src/main.rs
@@ -1,7 +1,17 @@
 use day_1::load_data;
 
 fn main() {
-    let elves = load_data();
+    let mut elves = load_data();
     let max_calories_elf = elves.iter().max_by_key(|elf| elf.total_calories).unwrap();
     println!("Part 1: {:?}", max_calories_elf);
+
+    elves.sort_by_key(|elf| elf.total_calories);
+    let num_elves = elves.len();
+    let top_3: Vec<u32> = elves[num_elves - 3..]
+        .iter()
+        .map(|elf| elf.total_calories)
+        .collect();
+    let total: u32 = top_3.iter().sum();
+
+    println!("Part 2: {:?}", total);
 }
